47dbf823307c29bc6ad5097a5d4f066627be4540
unknown <>
2025-02-15 18:55:25 +0200

    add

--- asset/js/app.js
+++ asset/js/app.js
@@ -8,3 +8,18 @@ icon.addEventListener("click", () => {
 close.addEventListener("click", () => {
   cart.classList.toggle("active");
 });
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw console.error(`Response status: ${response.status}`);
+    }
+    const date = await parent(response.json());
+    return date;
+  } catch {
+    console.error(`Response status: ${response.status}`);
+  }
+}
+
+console.log(getData("../products.json"));
